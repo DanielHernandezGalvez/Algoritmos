ebb12184b1cc4411b87265aeeeafef09a87322e6
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-18 12:47:39 -0600

    tipos literales

--- CT/tiposAvanzados.ts
+++ CT/tiposAvanzados.ts
@@ -15,3 +15,31 @@ type Usuario = {
 };
 
 let animal: Animal | Usuario = { id: 1, estado: "", name: "" };
+
+function sumaDos(n: number | string): number {
+  if (typeof n === "number") {
+    return n + 2;
+  }
+  return parseInt(n) + 2;
+}
+
+// console.log(sumaDos("2"));
+
+type Audit = {
+  created_at: string;
+  modified_at: string;
+};
+
+type Product = {
+  name: string;
+};
+
+const product: Audit & Product = {
+  created_at: "",
+  modified_at: "",
+  name: "",
+};
+
+// Literales
+type Fibo = 0 | 1 | 2 | 3 | 5;
+const nDFibo: Fibo = 1;
