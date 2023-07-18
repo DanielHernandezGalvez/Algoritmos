aadbbdacda2e2952278661d3663920c8cd0c6850
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-18 12:07:42 -0600

    funciones

--- CT/objetos.ts
+++ CT/objetos.ts
@@ -4,3 +4,25 @@ const objeto: {
 } = { id: 1, nombre: "" };
 
 // objeto.id = 23;
+type Direccion = {
+  numero: number;
+  calle: string;
+  pais: string;
+};
+type Persona = {
+  readonly id: number;
+  nombre: string;
+  direccion: Direccion;
+};
+
+const objetoDos: Persona = {
+  id: 1,
+  nombre: "Samantha",
+  direccion: {
+    numero: 2222,
+    calle: "colinas",
+    pais: "Samuelandia",
+  },
+};
+
+const arr: Persona[] = [];
