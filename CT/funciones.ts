0e2b2fbe6b6272e0630f17b4da7350b965b04d63
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-18 12:26:58 -0600

    tsconfig

--- CT/funciones.ts
+++ CT/funciones.ts
@@ -1,3 +1,23 @@
-const fn = () => {};
+const fn: () => number = () => {
+  let x = 2;
+  if (x > 5) {
+    return 7;
+  } else {
+    return 4;
+  }
+};
 
-function fn1() {}
+const fn2: (a: number) => string = (edad: number) => {
+  if (edad > 17) return "puedes ingresar";
+  return "no puedes pasar";
+};
+
+function validaEdad(edad: number, msg = "default"): string {
+  // quiero que me devuelva un string
+  if (edad > 17) {
+    return `puedes ingresar ${msg}`;
+  }
+  return "no puedes pasar";
+}
+
+console.log(validaEdad(18, "Hola soy Dani"));
---
+++ CT/objetos.js
@@ -0,0 +1,12 @@
+"use strict";
+const objeto = { id: 1, nombre: "" };
+const objetoDos = {
+    id: 1,
+    nombre: "Samantha",
+    direccion: {
+        numero: 2222,
+        calle: "colinas",
+        pais: "Samuelandia",
+    },
+};
+const arr = [];
