65b117590aa2108d988d4d2fefe3d6772a959e06
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-02 10:20:03 -0600

    getters setters

--- CT/POO.ts
+++ CT/POO.ts
@@ -6,6 +6,7 @@ class Personaje {
   // nivel: number;
   // private _hp: number; // solo se accede desde aquí
   profesion?: string; // lo hace opcional para agregar despues
+  private static equipo: number = 0;
   constructor(
     public readonly id: number,
     public name: string,
@@ -29,11 +30,34 @@ class Personaje {
     // no pasarse del máximo
     return this._hp;
   }
+
+  // Getter
+  get hp(): number {
+    return this._hp;
+  }
+
+  // Setter no puedeen retornar un valor
+  set hp(cantidad: number) {
+    this._hp = this._hp + cantidad;
+  }
+
+  static agregarPersonaje(): void {
+    Personaje.equipo++;
+  }
+
+  static getEquipo(): number {
+    return Personaje.equipo;
+  }
 }
 
 const personaje = new Personaje(1, "Charizard", 1, 100);
 personaje.cambiarHP(-10);
-console.log(personaje);
+personaje.hp = 20;
+// console.log(personaje);
+
+const personaje1 = new Personaje(2, "Rayquaza", 1, 120);
+Personaje.agregarPersonaje();
+console.log(Personaje.getEquipo());
 
 // if (personaje instanceof Personaje) {
 //   // puede ser true o false
